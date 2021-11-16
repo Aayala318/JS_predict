695b456546d21a9653a14494b3120406fd52ddcd
Aayala318 <adamayala1809@yahoo.com>
2021-11-16 12:09:35 -0800

    First Commit

---
+++ JSPredict.js
@@ -0,0 +1,21 @@
+//PREDICT1//
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    }
+    //Ans: It would log, 'I was born in, 1980'//
+
+//PREDICT2//
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+    }
+    //Ans: It would log, 'I was born in 1980' if birthYearInput= 1980//
+
+//PREDICT3//
+function add(num1, num2){
+        console.log("Summing Numbers!");              //var num1 = 10//
+        console.log("num1 is: " + num1);             //var num2 = 20//
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    //Ans: It would log, 'Summing Numbers!,num1 is 10,num2 is 20,30'
